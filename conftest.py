8628ecec2eaf5cfb8e9e0c1b970f08855b922db5
abhisheks <abhishekswarnkr@gmail.com>
2025-09-13 07:29:36 +0530

    little change3

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@ import pytest
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
-def browser():
+def launch_browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
 
 @pytest.fixture
-def page(browser):
-    page = browser.new_page()
+def page(launch_browser):
+    page = launch_browser.new_page()
     yield page
     page.close()
